f8d0e4c631b9eb622c6a5a4e61e2f3678ac0be70
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2022-09-09 01:55:18 +0530

    Hotel package list page and route added

--- src/routes/app-routes.jsx
+++ src/routes/app-routes.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 
 // Pages
-import { Sample, HotelOwnerLogin, Home } from "../pages";
+import { Sample, HotelOwnerLogin, Home, HotelPackageList } from "../pages";
 
 const AppRoutes = () => {
 	return (
@@ -16,6 +16,7 @@ const AppRoutes = () => {
 					<Route path="/" element={<Home />} />
 					<Route path="/sample" element={<Sample />} />
 					<Route path="/hotel-login" element={<HotelOwnerLogin />} />
+					<Route path="/hotel-package" element={<HotelPackageList />} />
 				</Routes>
 			</Router>
 		</>
