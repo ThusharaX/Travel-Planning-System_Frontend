dcdf1e9875648c0f4fb77f06b91382078bb195b6
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-10-08 00:19:45 +0530

    Tour Guide Login Complete

--- src/pages/tour-guide-login/index.jsx
+++ src/pages/tour-guide-login/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import TourGuideLogin from "./TourGuideLogin";
 
+import { TourGuideProvider } from "../../contexts/TourGuideContext";
+
 const index = () => {
 	return (
 		<>
-			<TourGuideLogin />
+			<TourGuideProvider>
+				<TourGuideLogin />
+			</TourGuideProvider>
 		</>
 	);
 };
