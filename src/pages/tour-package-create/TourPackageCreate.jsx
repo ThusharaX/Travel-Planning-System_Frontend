1fa360a59fffd3886cb74fb6dacd15c229619734
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-10-11 13:41:00 +0530

    Tour Package list

--- src/pages/tour-package-create/TourPackageCreate.jsx
+++ src/pages/tour-package-create/TourPackageCreate.jsx
@@ -29,141 +29,145 @@ const TourPackageCreate = () => {
 			<h1 className="mt-5 text-4xl text-center">Tour Package Create</h1>
 			<br></br>
 			<center>
-				<div>
-					<div className="block p-8 rounded-3xl shadow-lg bg-white max-w-screen-md max-h-full">
-						<form onSubmit={handleSubmit}>
-							<div className="grid grid-cols-2 gap-x-10">
-								<div className="form-group mb-6">
-									<label className="labelClass" htmlFor="fname">
-										Tour Package Name
-									</label>
+				{isLoading ? (
+					<h1 className="text-center">Loading...</h1>
+				) : (
+					<div>
+						<div className="block p-8 rounded-3xl shadow-lg bg-white max-w-screen-md max-h-full">
+							<form onSubmit={handleSubmit}>
+								<div className="grid grid-cols-2 gap-x-10">
+									<div className="form-group mb-6">
+										<label className="labelClass" htmlFor="fname">
+											Tour Package Name
+										</label>
 
+										<div className="flex ...">
+											<input
+												className="form-control block w-80 px-3 py-1.5 text-base border-indigo-500 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+												id="tourPackageName"
+												type="text"
+												aria-describedby="emailHelp123"
+												placeholder="Enter Tour Package Name"
+											></input>
+										</div>
+									</div>
+
+									<div className="form-group mb-6">
+										<label htmlFor="fname">Tour Guide Name</label>
+										<div className="flex ...">
+											<input
+												className="form-control block w-80 px-3 py-1.5 text-base border-indigo-500 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+												id="guideName"
+												type="text"
+												aria-describedby="emailHelp124"
+												placeholder="Enter Tour Guide Name"
+											></input>
+										</div>
+									</div>
+								</div>
+								<div className="grid grid-cols-2 gap-x-10">
+									<div className="form-group mb-10">
+										<label htmlFor="fname">Email</label>
+										<div className="flex ...">
+											<input
+												className="form-control block w-80 px-3 py-1.5 text-base  border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+												id="email"
+												type="email"
+												aria-describedby="emailHelp123"
+												placeholder="Enter Email Address"
+											></input>
+										</div>
+									</div>
+
+									<div className="form-group mb-6">
+										<label htmlFor="fname">Contact Number</label>
+										<div className="flex ...">
+											<input
+												className="form-control block  w-80 px-3 py-1.5 text-base border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+												id="contactNumber"
+												type="number"
+												aria-describedby="emailHelp124"
+												placeholder="Enter Contact Number"
+											></input>
+										</div>
+									</div>
+								</div>
+
+								<div className="grid grid-cols-2 gap-x-10">
+									<div className="form-group mb-10">
+										<label htmlFor="fname">Package Price</label>
+										<div className="flex ...">
+											<input
+												className="form-control block w-80 px-3 py-1.5 text-base  border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+												id="price"
+												type="number"
+												aria-describedby="emailHelp123"
+												placeholder="Pacakge Price"
+											></input>
+										</div>
+									</div>
+
+									<div className="form-group mb-6">
+										<label htmlFor="fname">Number of Days</label>
+										<div className="flex ...">
+											<input
+												className="form-control block  w-80 px-3 py-1.5 text-base border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+												id="NumberOfDays"
+												type="number"
+												aria-describedby="emailHelp124"
+												placeholder="Number of Days"
+											></input>
+										</div>
+									</div>
+								</div>
+
+								<div className="singleInput">
+									<label htmlFor="fname">Location</label>
 									<div className="flex ...">
-										<input
-											className="form-control block w-80 px-3 py-1.5 text-base border-indigo-500 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											id="tourPackageName"
-											type="text"
-											aria-describedby="emailHelp123"
-											placeholder="Enter Tour Package Name"
-										></input>
+										<div className="form-group mb-4">
+											<input
+												className="form-control singleInput block w-80 px-3 py-1.5 text-base  border-indigo-500 font-normal text-gray-700  bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+												id="location"
+												type="text"
+												aria-describedby="emailHelp124"
+												placeholder="Travel Location"
+											></input>
+										</div>
 									</div>
 								</div>
 
-								<div className="form-group mb-6">
-									<label htmlFor="fname">Tour Guide Name</label>
-									<div className="flex ...">
-										<input
-											className="form-control block w-80 px-3 py-1.5 text-base border-indigo-500 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											id="guideName"
-											type="text"
-											aria-describedby="emailHelp124"
-											placeholder="Enter Tour Guide Name"
-										></input>
-									</div>
-								</div>
-							</div>
-							<div className="grid grid-cols-2 gap-x-10">
-								<div className="form-group mb-10">
-									<label htmlFor="fname">Email</label>
-									<div className="flex ...">
-										<input
-											className="form-control block w-80 px-3 py-1.5 text-base  border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											id="email"
-											type="email"
-											aria-describedby="emailHelp123"
-											placeholder="Enter Email Address"
-										></input>
-									</div>
+								<label htmlFor="fname">Description</label>
+
+								<textarea
+									id="description"
+									rows="4"
+									className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+									placeholder="Describe Here Package"
+								></textarea>
+								<br></br>
+								<label htmlFor="fname">Upload File</label>
+
+								<div className="mb-4">
+									<input
+										className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+										id="images"
+										type="file"
+										multiple
+									/>
 								</div>
 
-								<div className="form-group mb-6">
-									<label htmlFor="fname">Contact Number</label>
-									<div className="flex ...">
-										<input
-											className="form-control block  w-80 px-3 py-1.5 text-base border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											id="contactNumber"
-											type="number"
-											aria-describedby="emailHelp124"
-											placeholder="Enter Contact Number"
-										></input>
-									</div>
+								<div className="mb-6 text-center">
+									<button
+										className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+										type="submit"
+									>
+										Submit
+									</button>
 								</div>
-							</div>
-
-							<div className="grid grid-cols-2 gap-x-10">
-								<div className="form-group mb-10">
-									<label htmlFor="fname">Package Price</label>
-									<div className="flex ...">
-										<input
-											className="form-control block w-80 px-3 py-1.5 text-base  border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											id="price"
-											type="number"
-											aria-describedby="emailHelp123"
-											placeholder="Pacakge Price"
-										></input>
-									</div>
-								</div>
-
-								<div className="form-group mb-6">
-									<label htmlFor="fname">Number of Days</label>
-									<div className="flex ...">
-										<input
-											className="form-control block  w-80 px-3 py-1.5 text-base border-indigo-500 font-normal  text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											id="NumberOfDays"
-											type="number"
-											aria-describedby="emailHelp124"
-											placeholder="Number of Days"
-										></input>
-									</div>
-								</div>
-							</div>
-
-							<div className="singleInput">
-								<label htmlFor="fname">Location</label>
-								<div className="flex ...">
-									<div className="form-group mb-4">
-										<input
-											className="form-control singleInput block w-80 px-3 py-1.5 text-base  border-indigo-500 font-normal text-gray-700  bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-											id="location"
-											type="text"
-											aria-describedby="emailHelp124"
-											placeholder="Travel Location"
-										></input>
-									</div>
-								</div>
-							</div>
-
-							<label htmlFor="fname">Description</label>
-
-							<textarea
-								id="description"
-								rows="4"
-								className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-								placeholder="Describe Here Package"
-							></textarea>
-							<br></br>
-							<label htmlFor="fname">Upload File</label>
-
-							<div className="mb-4">
-								<input
-									className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="images"
-									type="file"
-									multiple
-								/>
-							</div>
-
-							<div className="mb-6 text-center">
-								<button
-									className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-									type="submit"
-								>
-									Submit
-								</button>
-							</div>
-						</form>
+							</form>
+						</div>
 					</div>
-				</div>
+				)}
 			</center>
 		</>
 	);
--- src/pages/tour-package-list/TourPackageList.jsx
+++ src/pages/tour-package-list/TourPackageList.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import TourPackageContext from "../../contexts/TourPackageContext";
+import "../tour-package-list/TourPackageList.css";
 
 const TourPackageList = () => {
 	const { isLoading, tourPackages } = useContext(TourPackageContext);
@@ -7,41 +8,65 @@ const TourPackageList = () => {
 	return (
 		<>
 			<h1 className="mt-5 text-4xl text-center">Tour Package List</h1>
+			<br></br>
 
 			{isLoading ? (
 				<h1 className="text-center">Loading...</h1>
 			) : (
-				<div className="flex flex-wrap justify-center">
-					{tourPackages.map((tourPackage) => (
-						<div key={tourPackage._id} className="w-1/3 m-5 p-5 border border-gray-300 rounded-lg">
-							<h1 className="text-2xl">
-								<b>Package Name :</b> {tourPackage.tourPackageName}
-							</h1>
-							<h1 className="text-2xl">
-								<b>Guide Name :</b> {tourPackage.guideName}
-							</h1>
-							<h1 className="text-2xl">
-								<b>Email:</b> {tourPackage.email}
-							</h1>
-							<h1 className="text-2xl">
-								<b>Contact Number :</b> {tourPackage.contactNumber}
-							</h1>
-							<h1 className="text-2xl">
-								<b>Pacakge Price :</b> {tourPackage.price}
-							</h1>
-							<h1 className="text-2xl">
-								<b>Number of Days :</b> {tourPackage.NumberOfDays}
-							</h1>
-							<h1 className="text-2xl">
-								<b>Location :</b> {tourPackage.location}
-							</h1>
-							<h1 className="text-2xl">
-								<b>Description : </b> {tourPackage.description}
-							</h1>
-							<h1 className="text-2xl">{tourPackage.images}</h1>
-						</div>
-					))}
-				</div>
+				<center>
+					<table>
+						<thead className="bg-gray-50 border-b-2 border-gray-200 text-blue-400">
+							<tr>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Tour Package Name</th>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Tour Guide Name</th>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Email</th>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Contact Number</th>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Price</th>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Location</th>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Edit</th>
+								<th className="p-2 text-lg font-semibold tracking-wider text-left">Delete</th>
+							</tr>
+						</thead>
+						{tourPackages.map((tourPackages) => (
+							<tbody>
+								<tr className="bg-gray-50  ">
+									<td className="p-2 text-base font-semibold tracking-wide text-left">
+										{tourPackages.tourPackageName}
+									</td>
+									<td className="p-2 text-base font-semibold tracking-normal text-left">{tourPackages.guideName}</td>
+									<td className="p-2 text-base font-semibold tracking-normal text-left">{tourPackages.email}</td>
+									<td className="p-2 text-base font-semibold tracking-normal text-left">
+										{tourPackages.contactNumber}
+									</td>
+									<td className="p-2 text-base font-semibold tracking-normal text-left">{tourPackages.price}</td>
+									<td className="p-2 text-base font-semibold tracking-normal text-left">{tourPackages.location}</td>
+									<td className="p-2 whitespace-nowrap tracking-normal">
+										<img src="../public/pen-to-square-regular.svg" width="18" height="18" alt="Alex Shatov"></img>
+									</td>
+
+									<td className="p-2 whitespace-nowrap">
+										<button className="" onClick={() => deleteCampingPackage(campingPackage._id)}>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												className="h-5 w-5 mr-2"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+											>
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													strokeWidth="2"
+													d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+												/>
+											</svg>
+										</button>
+									</td>
+								</tr>
+							</tbody>
+						))}
+					</table>
+				</center>
 			)}
 		</>
 	);
