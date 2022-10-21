f1b04c758c9213730ec39c20597fa30cbf96da92
Shanuka Prabodha <shanukakariyawasam3@gmail.com>
2022-10-21 17:06:13 +0530

    fix login image position

--- src/pages/camping-vendor-login/CampingVendorLogin.jsx
+++ src/pages/camping-vendor-login/CampingVendorLogin.jsx
@@ -21,7 +21,7 @@ const CampingVendorLogin = () => {
 			<h1 className="mt-5 text-4xl text-center">Camping Vendor Login</h1>
 
 			<form onSubmit={handleSubmit}>
-				<div className="flex justify-center h-full max-w-4xl mx-auto mt-20 bg-white rounded-lg shadow-xl">
+				<div className="flex justify-center h-full mt-1 max-w-4xl mx-auto mt-20 bg-white rounded-lg shadow-xl">
 					<div className=" left bg-primary-blue  rounded-lg">
 						<div className="mt-32 ml-16 ">
 							<h1 className="text-5xl text-white ">
